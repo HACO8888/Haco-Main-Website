294489f66e5f386ebbcc1f68c1b0d6103a7dd110
HACO <jasonytonlinecomeandsee@gmail.com>
2023-05-02 19:06:42 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,30 +27,30 @@ export default function Home() {
               style={{ width: '20%', height: 'auto', "max-width": '128px' }}
               alt="logo"
               priority="true"
-              className="flex-initial"
+              className="flex-initial sm:ml-5"
             />
             <div className="flex flex-col max-w-full gap-3 m-auto ml-5 align-middle">
               <h1 className="text-5xl text-left max-md:text-3xl">HACO</h1>
               <Link href="mailto:jasonytonlinecomeandsee@gmail.com" target="_blank">
-                <p className="underline opacity-50 decoration-dashed hover:opacity-80 max-sm:hidden">
+                <p className="opacity-50 hover:opacity-80 max-sm:hidden">
                   jasonytonlinecomeandsee@gmail.com
                 </p>
-                <p className="underline opacity-50 decoration-dashed hover:opacity-80 sm:hidden">
-                  jasonytonlinecomeandsee......
+                <p className="opacity-50 hover:opacity-80 sm:hidden">
+                  jasonytonlinecomeandsee@gmail......
                 </p>
               </Link>
             </div>
             <div className="items-center flex-initial m-auto text-center align-middle max-md:flex-none max-md:hidden">
               <div className="flex flex-col gap-3">
-                <button className="w-auto px-4 py-3 text-white rounded-lg bg-info">📩 合作邀約</button>
-                <button className="px-4 py-3 text-white rounded-lg bg-success">🗂️ 專案製作</button>
+                <Link href="mailto:jasonytonlinecomeandsee@gmail.com" className="w-auto px-4 py-3 text-white rounded-lg bg-info" target="_blank">📩 合作邀約</Link>
+                <Link href="https://www.lazco.dev/contact" className="px-4 py-3 text-white rounded-lg bg-success" target="_blank">🗂️ 專案製作</Link>
               </div>
             </div>
           </div>
           <div className="items-center m-auto mt-10 text-center align-middle md:hidden">
             <div className="flex flex-col gap-3">
-              <button className="w-auto px-4 py-3 text-white rounded-lg bg-info" onClick={() => router.push('mailto:jasonytonlinecomeandsee@gmail.com')}>📩 合作邀約</button>
-              <button className="px-4 py-3 text-white rounded-lg bg-success">🗂️ 專案製作</button>
+              <Link href="mailto:jasonytonlinecomeandsee@gmail.com" className="w-auto px-4 py-3 text-white rounded-lg bg-info" target="_blank">📩 合作邀約</Link>
+              <Link href="https://www.lazco.dev/contact" className="px-4 py-3 text-white rounded-lg bg-success" target="_blank">🗂️ 專案製作</Link>
             </div>
           </div>
         </div>
